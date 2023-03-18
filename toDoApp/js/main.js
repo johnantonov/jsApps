c940963514e95e6d97452025273ed139d76378d2
Ivan Antonov <ivan.antonov.pro@gmail.com>
2023-03-19 00:41:27 +0600

    finished todo-app

--- toDoApp/js/main.js
+++ toDoApp/js/main.js
@@ -1,133 +1,122 @@
-document.addEventListener('DOMContentLoaded', (function (){
-    if(!localStorage.getItem('ToDoStorage')) {
-        localStorage.setItem('ToDoStorage', JSON.stringify([]))
-    }
-    if(!localStorage.getItem('DoneStorage')) {
-        localStorage.setItem('DoneStorage', JSON.stringify([]))
-    }
-    document.body.style.background = '#949693'
-    let app = document.querySelector('.app')
-    app.classList.add('d-flex', 'flex-wrap', 'justify-content-between')
-
-    
-    let title = document.createElement('h1')
-    title.textContent = "Список моих дел"
+document.addEventListener('DOMContentLoaded', (async function() {
+    title = document.createElement('h1')
+    title.classList.add('mb-5', 'mt-5')
+    title.textContent = 'Список дел' 
     title.style.textAlign = 'center'
-    title.classList.add('mb-5', 'col-12')
-    app.append(title)
+    input = document.createElement('input')
+    input.classList.add('form-control', 'mb-3')
+    btn = document.createElement('button')
+    btn.classList.add('btn', 'btn-outline-warning', 'mb-5')
+    btn.textContent = 'Создать дело'
+    btn.style.width = '30%'
+    btn.style.fontSize = '20px'
+    btn.style.alignSelf = 'center'
 
-    let formCreate = document.createElement('form')
-    formCreate.classList.add('input-group', 'mb-5')
-    app.append(formCreate)
+    lists = document.createElement('div')
+    lists.classList.add('d-flex', 'justify-content-between')
 
-    let input = document.createElement('input')
-    input.textContent = "Список моих дел"
-    input.classList.add('form-control') 
-    formCreate.append(input)
+    doList = document.createElement('ul')
+    doneList = document.createElement('ul')
+    doList.style.marginRight = '10px'
 
-    let buttonCreate = document.createElement('input')
-    buttonCreate.textContent = "Создать дело"
-    buttonCreate.type = 'submit'
-    buttonCreate.classList.add('btn', 'btn-primary', 'btn-lg')
-    formCreate.append(buttonCreate)
+    doCap = document.createElement('li')
+    doCap.innerHTML = '<h3 class="display-6 mb-5"><b>Дела</b></h3>'
+    doCap.style.marginLeft = '50%'
+    doList.append(doCap)
+    doneCap = document.createElement('li')
+    doneCap.innerHTML = '<h3 class="display-6 mb-5"><b>Завершенные дела</b></h3>'
+    doneList.append(doneCap)
 
-    let doList = document.createElement('div', 'col-6') 
-    doList.style.maxWidth = '45%'
-    let doListTitle = document.createElement('h2')
-    doListTitle.textContent = 'Дела'
-    doListTitle.style.marginLeft = '30%'
-    doListTitle.style.marginBottom = '50px'
-    let doUl = document.createElement('ul')
-    doList.append(doListTitle)
-    doList.append(doUl)
-    app.append(doList)
+    lists.append(doList, doneList)
 
-    let doneList = document.createElement('div', 'col-6') 
-    doneList.style.maxWidth = '45%'
-    let doneListTitle = document.createElement('h2')
-    doneListTitle.textContent = 'Завершенные дела'
-    doneListTitle.style.marginRight = '30%'
-    doneListTitle.style.marginBottom = '50px'
-    let doneUl = document.createElement('ul')
-    doneList.append(doneListTitle)
-    doneList.append(doneUl)
-    app.append(doneList)
-
-    createToDoItem()
-
-    function createToDoItem() {
-        storage = JSON.parse(localStorage.getItem('ToDoStorage'))
-        for (let i = 0; i < storage.length; ++i) {
-            doLi = document.createElement('li')
-            doLi.id = `doLi${i}`
-            doLi.style.marginBottom = '30px'
-            doLi.style.overflowWrap = 'break-word'
-            doLi.style.maxWidth = '500px'
-            doLi.style.padding = '5px'
-            doLi.style.border = '3px solid #564345'
+    document.body.classList.add('d-flex', 'flex-column')
+    document.body.append(title, input, btn, lists)
     
-            doLiText = document.createElement('p')
-            doLiText.textContent = storage[i]
-            doLiText.style.fontSize = '25px'
-            doLiText.style.marginBottom = '10px'
-    
-            let btnDone = document.createElement('button')
-            btnDone.classList.add('btn', 'btn-success', 'btn-lg')
-            btnDone.id = `btnDone${i}`
-            btnDone.style.marginBottom = '5%'
-            btnDone.textContent = 'Завершить'
-    
-            let btnDel = document.createElement('button')
-            btnDel.classList.add('btn', 'btn-danger', 'btn-lg')
-            btnDel.id = `btnDel${i}`
-            btnDel.style.margin = '0 5% 5% 0'
-            btnDel.textContent = 'Удалить'
-    
-    
-            doLi.append(doLiText, btnDel, btnDone)
-            doUl.append(doLi)
 
-    
-            btnDel.addEventListener('click', function(e) {
-                document.getElementById(`doLi${e.target.id[e.target.id.length-1]}`).remove()
-                // storage = JSON.parse(localStorage.getItem('ToDoStorage'))
-                // storage.splice([e.target.id[e.target.id.length-1]], 1)
-                // localStorage.setItem('ToDoStorage', JSON.stringify(storage))
+    async function updateAfter() {
+        const response = await fetch ('http://localhost:3000/api/todos')
+        const todoStorageList = await response.json()
+        console.log(todoStorageList)
+        todoStorageList.forEach(todoItem => updateList(todoItem))
+    }
+    updateAfter()
 
-            })
-    
-            btnDone.addEventListener('click', function(e) {
-                doneUl.append(document.getElementById(`doLi${e.target.id[e.target.id.length-1]}`))
-                document.getElementById(`btnDone${e.target.id[e.target.id.length-1]}`).remove() 
-            })
+    function updateList({name, done, id}) {
+        doElem = document.createElement('li')
+        doElem.classList.add(`classRemove${id}`)
+        doElemText = document.createElement('p')
+        doElemText.textContent = name
+        doElem.classList.add('display-6', 'mb-3', 'd-flex', 'flex-column')
+        doneBtn = document.createElement('button')
+        doneBtn.classList.add('mb-1', 'btn', 'btn-outline-success', 'btn-lg')
+        doneBtn.textContent = 'Завершить'
+        doneBtn.id = `${id}`
+        delBtn = document.createElement('button')
+        delBtn.classList.add('btn', 'btn-outline-danger', 'btn-lg')
+        delBtn.textContent = 'Удалить'
+        delBtn.id = `${id*2}`
 
-            //     doneStorage = JSON.parse(localStorage.getItem('DoneStorage'))
-            //     doneStorage.push([storage][e.target.id[e.target.id.length-1]])
-            //     localStorage.setItem('DoneStorage', JSON.stringify(doneStorage))
-
-            //     storage = JSON.parse(localStorage.getItem('ToDoStorage'))
-            //     storage.splice([e.target.id[e.target.id.length-1]], 1)
-            //     localStorage.setItem('ToDoStorage', JSON.stringify(storage))
-
-                
-                
-            // })
+        if (done) {
+            doElem.append(doElemText, delBtn)
+            doneList.append(doElem)
+        } else {
+            doElem.append(doElemText, doneBtn, delBtn)
+            doList.append(doElem)
         }
+
+        doneBtn.addEventListener('click', (e) => {
+            e.preventDefault()
+            fetch(`http://localhost:3000/api/todos/${e.target.id}`, {
+                method: 'PATCH', 
+                body: JSON.stringify({done: true})
+            })
+            // document.querySelector(e.target.id).remove()
+            doneElem = document.querySelector(`.classRemove${e.target.id}`)
+            doneList.append(doneElem)
+            document.getElementById(e.target.id).remove()
+        })
+
+        delBtn.addEventListener('click', (e) => {
+            e.preventDefault()
+            fetch(`http://localhost:3000/api/todos/${e.target.id/2}`, {
+                method: 'DELETE', 
+            })
+            document.querySelector(`.classRemove${e.target.id/2}`).remove()
+        })
     }
 
-    buttonCreate.addEventListener('click', function() {
-        if (input.value === '') {
+
+
+
+
+
+    btn.addEventListener('click', async e => {
+        e.preventDefault();
+
+        if (!input.value) {
             return
         }
-        storage = JSON.parse(localStorage.getItem('ToDoStorage'))
-        storage.push(input.value)
-        localStorage.setItem('ToDoStorage', JSON.stringify(storage))
-        createToDoItem()
+
+        console.log(input.value)
+        const response = await fetch ('http://localhost:3000/api/todos', {
+            method: 'POST', 
+            body: JSON.stringify({
+                name: input.value,
+                owner: 'John'
+            }),
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        })
+
+        const todoItem = await response.json()
+        updateList(todoItem)
 
         input.value = ''
     })
-    
+
 
     
+
 
 }))
